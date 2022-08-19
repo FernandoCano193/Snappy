bcd2af728d714fd5f68034fb2579fe295fbe3f75
Fernando Cano <jose.cano193@tectijuana.edu.mx>
2022-08-18 19:35:32 -0700

    Se agrego el carruselView en el intro

---
+++ snappyAPP/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using snappyAPP.Views;
+
+namespace snappyAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Presentacion();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
